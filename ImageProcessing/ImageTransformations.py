2d053a5c364cff677f82d2ecd32a3ff05d90a800
Wizard-Mayank <30517csaiml@gmail.com>
2025-08-05 00:10:14 +0530

    image transformations

--- ImageProcessing/ImageTransformations.py
+++ ImageProcessing/ImageTransformations.py
@@ -8,26 +8,30 @@ soldiers = cv.imread("E:\Python Projects\Computer_Vision\Assets\Images\Soldiers.
 resizedSoldiers = cv.resize(soldiers, (700, 600), interpolation=cv.INTER_AREA)
 cv.imshow("resized Soldiers", resizedSoldiers)
 
+
 # to shift(translate) image in any direction -x = Left, +x = R, -y = U, +y = Down
 def translate(img, x, y):
     transMat = np.float32([[1, 0, x], [0, 1, y]])
     dimensions = (img.shape[0], img.shape[1])
     return cv.warpAffine(img, transMat, dimensions)
 
+
 # shifting img towards Top Left corner
 # translatedSoldiers = translate(resizedSoldiers, -300, -100)
 # cv.imshow("Translated soldiers", translatedSoldiers)
 
+
 # to rotate image -angle = clockwise +angle = counter-clockwise
 def rotate(img, angle, rotPoint=None):
     (width, height) = img.shape[:2]
-    
-    if rotPoint==None:
-        rotPoint = (width//2, height//2)
-        
+
+    if rotPoint == None:
+        rotPoint = (width // 2, height // 2)
+
     rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
     dimensions = (width, height)
     return cv.warpAffine(img, rotMat, dimensions)
+
 
 rotatedSoldiers1 = rotate(resizedSoldiers, -45)
 cv.imshow("Rotated Soldiers 1", rotatedSoldiers1)
